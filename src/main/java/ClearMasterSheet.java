433da25e9e6aed4eb17383655a1d23c8604bca8f
Gauri Khandave <gaurikhandave858@gmail.com>
2026-01-13 11:14:59 +0530

    Update ClearMasterSheet utility with proper sheet handling logic

--- src/main/java/ClearMasterSheet.java
+++ src/main/java/ClearMasterSheet.java
@@ -1,10 +1,15 @@
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ClearMasterSheet {
     public static void main(String[] args) {
